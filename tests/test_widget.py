b101ecff954de8ce2c26164e2ae39a21e2cf9224
dj3747 <dj94911131@gmail.com>
2025-03-01 15:08:24 +0300

    finish fixed test_widget.py

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -8,6 +8,7 @@ from src.widget import get_date, mask_account_card
     [
         ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
         ("Maestro 7000792289606361", "Maestro 7000 79** **** 6361"),
+        ("Счет 73654108430135874305", "Счет **4305"),
     ],
 )
 def test_mask_account_card(card_number: str, expected_masked: str) -> None:
