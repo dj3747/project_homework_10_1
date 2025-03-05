042910f84fb9346e76f1afdcb86046dbe7cba071
dj3747 <dj94911131@gmail.com>
2025-03-06 01:02:58 +0300

    fixed generators.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.masks import get_mask_account, get_mask_card_number
 from src.processing import filter_by_state, sort_by_date, list_of_dictionaries
 from src.widget import get_date, mask_account_card
 
+
 if __name__ == "__main__":
     print(get_mask_card_number("7000792289606361"))
     print(get_mask_account("73654108430135874305"))
