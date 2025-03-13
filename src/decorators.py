f5ed530c539a27a12aec5d0c5e278043f2bbb595
dj3747 <dj94911131@gmail.com>
2025-03-13 17:13:52 +0300

    fixed test_decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,36 +1,40 @@
 import time
 from functools import wraps
 
+
 def log(filename=None):
     """Декоратор, который будет логировать начало и конец выполнения функции,
     а также её результаты и возникшие ошибки"""
 
     def my_decorator(func):
-            @wraps(func)
-            def wrapper(*args, **kwargs):
-                start_time = time.time()
-                try:
-                    result = func(*args, **kwargs)
-                    end_time = time.time()
-                    log_message = (
-                        f"{func.__name__} started at {start_time} and finished at {end_time} with result: {result}"
-                    )
-                    if filename is not None:
-                        with open(filename, "a", encoding="utf-8") as file:
-                            file.write(f"{func.__name__} ok\n")
-                    else:
-                        print(log_message)
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            try:
+                result = func(*args, **kwargs)
+                end_time = time.time()
+                log_message = (
+                    f"{func.__name__} started at {start_time} and finished at {end_time} with result: {result}"
+                )
+                if filename is not None:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} ok\n")
+                else:
+                    print(log_message)
+                print("my_function ok")
 
-                except Exception as e:
-                    log_message_1 = f"{func.__name__} error {e.__class__.__name__}. Inputs: {args}, {kwargs}"
-                    if filename:
-                        with open(filename, "a", encoding="utf-8") as file:
-                            file.write(log_message_1 + "\n")
-                    else:
-                        print(log_message_1)
-                    raise e
-                return result
-            return wrapper
+            except Exception as e:
+                log_message_1 = f"{func.__name__} error {e.__class__.__name__}. Inputs: {args}, {kwargs}\n"
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(log_message_1 + "\n")
+                else:
+                    print(log_message_1)
+                raise e
+            return result
+
+        return wrapper
+
     return my_decorator
 
 
@@ -38,5 +42,6 @@ def log(filename=None):
 def my_function(x, y):
     """Получение суммы двух чисел"""
     return x + y
+
 
 my_function(1, 2)
---
+++ tests/test_decorators.py
@@ -0,0 +1,22 @@
+import pytest
+from src.decorators import my_function
+from src.decorators import log
+
+
+@log()
+def my_function(x, y):
+    return x + y
+
+
+def test_my_function(capsys):
+    my_function(1, 2)
+    captured = capsys.readouterr()
+    assert "my_function ok" in captured.out
+
+
+def test_my_function_error_capsys(capsys):
+    with pytest.raises(TypeError):
+        my_function(1, "3")
+
+    captured = capsys.readouterr()
+    assert "my_function error TypeError" in captured.out
